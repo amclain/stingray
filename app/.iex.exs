365dfe0605177dc8684284515a123ab7ba70368e
Alex McLain <alex@alexmclain.com>
2022-11-20 20:12:09 -0800

    Add stinger to IEx prompt
    This helps differentiate which prompt the user is in when consoling
    into other devices.

--- app/.iex.exs
+++ app/.iex.exs
@@ -1,1 +1,4 @@
+IEx.configure(default_prompt: "%prefix(%counter)\e[1;34m=>\e[0m")
+IEx.configure(alive_prompt: "%prefix(%node)%counter\e[1;34m=>\e[0m")
+
 alias Stingray.Target
--- app/rootfs_overlay/etc/iex.exs
+++ app/rootfs_overlay/etc/iex.exs
@@ -6,6 +6,9 @@ logo = """
 \e[1;34m   ▀██▀          \e[0m
 """
 
+IEx.configure(default_prompt: "%prefix(%counter)\e[1;34m=>\e[0m")
+IEx.configure(alive_prompt: "%prefix(%node)%counter\e[1;34m=>\e[0m")
+
 NervesMOTD.print(logo: logo)
 
 # Add Toolshed helpers to the IEx session
