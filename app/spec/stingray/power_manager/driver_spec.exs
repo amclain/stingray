2f3d2b8339eb679c835a5b6b69c228e016af7afd
Alex McLain <alex@alexmclain.com>
2022-12-28 15:33:13 -0800

    Invert GPIO power signal

--- app/spec/stingray/power_manager/driver_spec.exs
+++ app/spec/stingray/power_manager/driver_spec.exs
@@ -9,7 +9,7 @@ defmodule Stingray.PowerManager.Driver.Test do
     Resolve.inject(Stingray.PowerManager, Stingray.PowerManager.Driver)
 
     Resolve.inject(Circuits.GPIO, quote do
-      def open(_pin, :output, [initial_value: 1]),
+      def open(_pin, :output, [initial_value: 0]),
         do: {:ok, self()}
 
       def open(_pin, _direction, _opts),
@@ -45,52 +45,52 @@ defmodule Stingray.PowerManager.Driver.Test do
     it "turns a target's power off and on if the power is already on" do
       :ok = PowerManager.on
       expect PowerManager.power? |> to(eq true)
-      assert_received {:gpio, :write, 0}
+      assert_received {:gpio, :write, 1}
 
       :ok = PowerManager.cycle(1)
       :timer.sleep(5)
       expect PowerManager.power? |> to(eq true)
 
-      assert_receive {:gpio, :write, 1}
       assert_receive {:gpio, :write, 0}
+      assert_receive {:gpio, :write, 1}
     end
 
     it "immediately turns a target's power on if the power is off" do
       :ok = PowerManager.off
       expect PowerManager.power? |> to(eq false)
-      assert_received {:gpio, :write, 1}
+      assert_received {:gpio, :write, 0}
 
       :ok = PowerManager.cycle(30)
       expect PowerManager.power? |> to(eq true)
 
-      refute_receive {:gpio, :write, 1}
-      assert_receive {:gpio, :write, 0}
+      refute_receive {:gpio, :write, 0}
+      assert_receive {:gpio, :write, 1}
     end
 
     it "cancels the cycle if power on is called" do
       :ok = PowerManager.on
       expect PowerManager.power? |> to(eq true)
-      assert_received {:gpio, :write, 0}
-
-      :ok = PowerManager.cycle(10)
       assert_received {:gpio, :write, 1}
 
-      :ok = PowerManager.on
+      :ok = PowerManager.cycle(10)
       assert_received {:gpio, :write, 0}
-      refute_receive {:gpio, :write, 0}
+
+      :ok = PowerManager.on
+      assert_received {:gpio, :write, 1}
+      refute_receive {:gpio, :write, 1}
     end
 
     it "cancels the cycle if power off is called" do
       :ok = PowerManager.on
       expect PowerManager.power? |> to(eq true)
-      assert_received {:gpio, :write, 0}
+      assert_received {:gpio, :write, 1}
 
       :ok = PowerManager.cycle(10)
-      assert_received {:gpio, :write, 1}
+      assert_received {:gpio, :write, 0}
 
       :ok = PowerManager.off
-      assert_received {:gpio, :write, 1}
-      refute_receive {:gpio, :write, 0}
+      assert_received {:gpio, :write, 0}
+      refute_receive {:gpio, :write, 1}
     end
   end
 end
